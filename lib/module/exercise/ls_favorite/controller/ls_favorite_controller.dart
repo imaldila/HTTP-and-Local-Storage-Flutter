fec3f97793edb869e81092a7219a265d1a37bdd3
Apit <apit@thezatapp.com>
2022-12-03 19:03:26 +0700

    LsFavorite

--- lib/module/exercise/ls_favorite/controller/ls_favorite_controller.dart
+++ lib/module/exercise/ls_favorite/controller/ls_favorite_controller.dart
@@ -22,6 +22,10 @@ class LsFavoriteController extends State<LsFavoriteView>
   bool ready = false;
   List productList = [];
   loadProductList() async {
+    productList = mainStorage.get("products");
+    setState(() {});
+    ready = true;
+    setState(() {});
     /*
     TODO: --
     1. Ambil data product dari local storage, gunakan kode ini:
@@ -40,6 +44,10 @@ class LsFavoriteController extends State<LsFavoriteView>
   }
 
   addToFavorite(Map item) {
+    item["favorite"] = !(item["favorite"] ?? false);
+    setState(() {});
+    saveToLocalStorage();
+    setState(() {});
     /*
     TODO: --
     4. Gunakan kode ini, untuk mengubah status product
